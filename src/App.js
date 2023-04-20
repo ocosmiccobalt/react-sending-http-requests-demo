9064f50b4aafa353a12fc3d48d9255994b13a5ab
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-04-20 12:33:19 +0300

    Add Movie & MoviesList components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { Fragment } from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 const App = () => {
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+
   return (
-    <div>
-      Hello!
-    </div>
+    <Fragment>
+      <section>
+        <button type='button'>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </Fragment>
   );
 };
 export default App;
