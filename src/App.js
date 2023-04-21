708ab80d6252b2b7730fb10458c91d8e4b384280
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-04-22 00:51:58 +0300

    Handle loading & data states

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
 
@@ -20,6 +22,7 @@ const App = () => {
     });
 
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -30,7 +33,9 @@ const App = () => {
         </button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </Fragment>
   );
