f2ac66a3c8173e9ed0c39bd28bd3b0cde93dd9f0
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-04-22 01:27:17 +0300

    Handle http errors

--- src/App.js
+++ src/App.js
@@ -6,23 +6,49 @@ import './App.css';
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
-      };
-    });
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
-    setMovies(transformedMovies);
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+      });
+
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message)
+    }
+
     setIsLoading(false);
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -32,11 +58,7 @@ const App = () => {
           Fetch Movies
         </button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </Fragment>
   );
 };
