808c587b9c13f3cc01457be22f14933d7b2cc5a5
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-04-25 16:51:14 +0300

    Handle http errors sending a POST request

--- src/App.js
+++ src/App.js
@@ -46,16 +46,26 @@ const App = () => {
   }, [fetchMoviesHandler, recentlyAdded]);
 
   async function addMovieHandler(movie) {
-    const response = await fetch('https://react-http-23-default-rtdb.firebaseio.com/movies.json', {
-      method: 'POST',
-      body: JSON.stringify(movie),
-      headers: {
-        'Content-Type': 'application/json'
+    setError(null);
+    try {
+      const response = await fetch('https://react-http-23-default-rtdb.firebaseio.com/movies.json', {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
       }
-    });
-    const data = await response.json();
-    console.log(data);
-    setRecentlyAdded((prevAdded) => ++prevAdded);
+
+      const data = await response.json();
+      console.log(data);
+      setRecentlyAdded((prevAdded) => ++prevAdded);
+    } catch (error) {
+      setError(error.message);
+    }
   }
 
   let content = <p>Found no movies.</p>;
